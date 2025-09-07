309396f133098e8ea7ea00de0446084ee8fda131
Ayou2529 <bladetinity@gmail.com>
2025-09-07 18:28:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 // config.js – global configuration for Mangpong PWA
-window.GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyNG1UV3xf8bMFLs9cr-JQbS7Q2VUw-PBQeFxX9nwW93s-XXwWslwN9TrhZML9VJqJv/exec";
+window.GOOGLE_SCRIPT_URL =
+  "https://script.google.com/macros/s/AKfycbx_FOvPbpEuGJZ8nf3v39v9RKyzLxW5G30oKwyViypHGaAaaJEL8J5fU1l8qiz4P7Tm/exec";
